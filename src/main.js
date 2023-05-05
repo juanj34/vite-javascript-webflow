12dc64bbbdafbe87e871dc00fb82b0c59e1fe666
juanj34 <juanjaramillo34@gmail.com>
2023-05-05 10:35:34 -0500

    Updated everything
    Updated with jsap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,110 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
 import './styles/style.css'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-console.log('Welcome to Vite + JS + Webflow!')
+// Move cursor
 
-createBadge()
-animateTitle()
+console.log('fuck')
+
+gsap.registerPlugin(ScrollTrigger)
+// Horizontal scroll
+let tlMain = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.section-height',
+      start: 'top top',
+      end: '98% bottom',
+      scrub: 1,
+    },
+  })
+  .to('.track', {
+    xPercent: -100,
+    ease: 'none',
+  })
+// hero photo
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.hero-panel',
+      containerAnimation: tlMain,
+      start: 'left left',
+      end: 'right left',
+      scrub: true,
+    },
+  })
+  .from('.hero-panel_img', { scale: 1.6 }, 0)
+
+// note
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.note-panel',
+      containerAnimation: tlMain,
+      start: 'left right',
+      end: 'left left',
+      scrub: true,
+    },
+  })
+  .from('.note-panel_img', { rotate: 45, scale: 0.3 })
+
+// thanks
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.thanks-panel_wrap',
+      containerAnimation: tlMain,
+      start: 'left left',
+      end: 'right right',
+      scrub: true,
+    },
+  })
+  .to('.thanks-panel', { xPercent: 100, ease: 'none' })
+  .to('.thanks-panel_photo', { scale: 1 }, 0)
+  .fromTo(
+    '.thanks-panel_contain.is-2',
+    {
+      clipPath: 'polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)',
+    },
+    { clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', ease: 'none' },
+    0
+  )
+
+// stagger photos
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.stagger-panel',
+      containerAnimation: tlMain,
+      start: 'left right',
+      end: 'right left',
+      scrub: true,
+    },
+  })
+  .from('.stagger-panel_img', { x: '100vw', stagger: { each: 0.05 } })
+  .to('.stagger-panel_img', { scale: 0.5, stagger: { each: 0.05 } })
+
+let tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.section_layout91',
+    start: 'top center+=50%',
+    end: 'bottom top',
+    scrub: true,
+  },
+})
+tl3.to('.header1_image', {
+  rotate: 90,
+  duration: 1,
+})
+
+let tlsecond = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.section_layout98',
+    start: 'top 80%',
+    end: 'bottom top',
+    scrub: true,
+  },
+})
+tlsecond.to('.layout98_image', {
+  scale: 1.5,
+  duration: 1,
+})
